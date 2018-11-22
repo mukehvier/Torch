bc7db08941fb2f5406b49254e6f4a6aa1701c6e6
Daniil Pakhomov <warmspringwinds@gmail.com>
2018-11-22 13:04:22 -0500

    table + accuracy resnet implementation for cifar

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pytorch-image-classification
+
 Image classification in pytorch on Cifar and Imagenet
+
+
+## Cifar-10 
+
+| Model            | Test data | Accuracy | Related paper |
+|------------------|-----------|---------|--------------------|
+| Resnet-20    | Cifar-10  | 0.9213 |  [Deep Residual Learning](https://arxiv.org/pdf/1512.03385.pdf) |
--- pytorch_image_classification/models/resnet.py
+++ pytorch_image_classification/models/resnet.py
@@ -1,117 +1,184 @@
 import torch
 import torch.nn as nn
 import math
+import numpy as np
 
-# Credits:
-# pytorch-cifar: https://github.com/kuangliu/pytorch-cifar/blob/master/models/preact_resnet.py
-# Tensoflow models: https://github.com/tensorflow/models/blob/master/research/slim/nets/resnet_v2.py
 
-class PreActBasicBlock(nn.Module):
+def conv3x3(in_planes, out_planes, stride=1, dilation=1):
+    "3x3 convolution with padding"
     
-    '''Pre-activation version of the BasicBlock.'''
+    kernel_size = np.asarray((3, 3))
     
+    # Compute the size of the upsampled filter with
+    # a specified dilation rate.
+    upsampled_kernel_size = (kernel_size - 1) * (dilation - 1) + kernel_size
+    
+    # Determine the padding that is necessary for full padding,
+    # meaning the output spatial size is equal to input spatial size
+    full_padding = (upsampled_kernel_size - 1) // 2
+    
+    # Conv2d doesn't accept numpy arrays as arguments
+    full_padding, kernel_size = tuple(full_padding), tuple(kernel_size)
+    
+    return nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
+                     padding=full_padding, dilation=dilation, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
-        super(PreActBlock, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_planes)
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+    def __init__(self, inplanes, planes, stride=1, downsample=None, dilation=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride, dilation=dilation)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes, dilation=dilation)
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
-            )
+        self.downsample = downsample
+        self.stride = stride
 
     def forward(self, x):
-        out = F.relu(self.bn1(x))
-        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
-        out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
-        out += shortcut
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
         return out
 
 
-class PreActBottleneck(nn.Module):
-    '''Pre-activation version of the original Bottleneck module.'''
+class Bottleneck(nn.Module):
+    
     expansion = 4
 
-    def __init__(self, in_planes, planes, stride=1):
-        super(PreActBottleneck, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_planes)
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+    def __init__(self, inplanes, planes, stride=1, downsample=None, dilation=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        
+        self.conv2 = conv3x3(planes, planes, stride=stride, dilation=dilation)
+        
         self.bn2 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-
-        if stride != 1 or in_planes != self.expansion*planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False)
-            )
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
 
     def forward(self, x):
-        out = F.relu(self.bn1(x))
-        shortcut = self.shortcut(out) if hasattr(self, 'shortcut') else x
-        out = self.conv1(out)
-        out = self.conv2(F.relu(self.bn2(out)))
-        out = self.conv3(F.relu(self.bn3(out)))
-        out += shortcut
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
         return out
-    
-class PreActResNetCifar(nn.Module):
-    
-    def __init__(self, block, num_blocks, num_classes=10):
-        
-        super(PreActResNetCifar, self).__init__()
-        self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
 
-    def _make_layer(self, block, planes, num_blocks, stride):
+class ResNetCifar(nn.Module):
+
+    def __init__(self, block, layers, num_classes=10):
         
-        strides = [stride] + [1]*(num_blocks-1)
+        super(ResNetCifar, self).__init__()
+        
+        self.inplanes = 16
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu = nn.ReLU(inplace=True)
+        
+        self.layer1 = self._make_layer(block, 16, layers[0])
+        self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        
+        self.fc = nn.Linear(64 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        
+        downsample = None
+        
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
         layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
         return nn.Sequential(*layers)
 
     def forward(self, x):
         
-        out = self.conv1(x)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
         
-        return out
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
 
-def PreActResNet18Cifar10():
-    
-    return PreActResNetCifar(PreActBlock, [2,2,2,2])
+        return x
 
-def PreActResNet34Cifar10():
+def ResNet20Cifar(nclass=10):
     
-    return PreActResNetCifar(PreActBlock, [3,4,6,3])
+    return ResNetCifar(BasicBlock, [3, 3, 3], num_classes=nclass)
 
-def PreActResNet50Cifar10():
+def ResNet32Cifar(nclass=10):
     
-    return PreActResNetCifar(PreActBottleneck, [3,4,6,3])
+    return ResNetCifar(BasicBlock, [5, 5, 5], num_classes=nclass)
 
-def PreActResNet101Cifar10():
+def ResNet44Cifar(nclass=10):
     
-    return PreActResNetCifar(PreActBottleneck, [3,4,23,3])
+    return ResNetCifar(BasicBlock, [7, 7, 7], num_classes=nclass)
 
-def PreActResNet152Cifar10():
+def ResNet56Cifar(nclass=10):
     
-    return PreActResNetCifar(PreActBottleneck, [3,8,36,3])
+    return ResNetCifar(BasicBlock, [9, 9, 9], num_classes=nclass)
+
+def ResNet110Cifar(nclass=10):
+    
+    return ResNetCifar(BasicBlock, [18, 18, 18], num_classes=nclass)
